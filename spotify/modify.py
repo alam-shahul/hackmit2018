e59fc4af612c0e6924cb47b70f7dad988398be5e
Nur Muhammad (Mahi) Shafiullah <n.m.mahi@gmail.com>
2018-09-15 14:36:29 -0400

    Fixed oauth token

--- spotify/modify.py
+++ spotify/modify.py
@@ -6,7 +6,7 @@ from BASIC_INFO import *
 from SECRETS import OAUTH_TOKEN_SELF
 
 def add_tracks_to_playlist(tracks):
-	sp = spotipy.Spotify(auth=token)
+	sp = spotipy.Spotify(auth=OAUTH_TOKEN_SELF)
 	sp.trace = False
 	results = sp.user_playlist_add_tracks(MY_USERNAME, PARTY_PLAYLIST, track_ids)
 	return (results)
