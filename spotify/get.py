756f5ec4f9b3ac1f2fe24626982128ecc83188a2
Jen Li <jingyuli@mit.edu>
2018-09-15 17:09:00 -0400

    get

--- spotify/get.py
+++ spotify/get.py
@@ -10,18 +10,17 @@ client_credentials_manager = SpotifyClientCredentials()
 SP = sp.Spotify(client_credentials_manager=client_credentials_manager)
 
 def get_playlists(user_id):
-    return SP.user_playlists(user_id)
+    playlists = SP.user_playlists(user_id)
+    return [pl['uri'].split(':')[-1] for pl in playlists['items']]
 
 def get_songs_from_playlist(user_id, playlist_id):
     playlist_tracks = SP.user_playlist_tracks(user_id, playlist_id, fields='items,uri,name,id,total', market='fr')
-    return playlist_tracks
+    return [pl['track']['id'] for pl in playlist_tracks['items']]
 
 def get_all_songs(user_id):
     playlists = get_playlists(user_id)
-    print (playlists)
     songs = []
     for pl in playlists:
-        print (pl)    	
         songs += get_songs_from_playlist(user_id, pl)
     return songs
 
