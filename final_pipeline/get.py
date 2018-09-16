39f59758fda425f6c3466f33b401691902f072ed
Shahul Alam <alam.shahul@gmail.com>
2018-09-15 21:36:15 -0400

    Modified get.py to account for cases in which a user has no playlists.

--- final_pipeline/get.py
+++ final_pipeline/get.py
@@ -11,11 +11,15 @@ SP = sp.Spotify(client_credentials_manager=client_credentials_manager)
 
 def get_playlists(user_id):
     playlists = SP.user_playlists(user_id)
-    return [pl['uri'].split(':')[-1] for pl in playlists['items']]
+    if 'items' in playlists:
+        return [pl['uri'].split(':')[-1] for pl in playlists['items']]
+    return []
 
 def get_songs_from_playlist(user_id, playlist_id):
     playlist_tracks = SP.user_playlist_tracks(user_id, playlist_id, fields='items,uri,name,id,total', market='fr')
-    return [pl['track']['id'] for pl in playlist_tracks['items']]
+    if 'items' in playlist_tracks:
+        return [pl['track']['id'] for pl in playlist_tracks['items']]
+    return []
 
 def get_all_songs(user_id):
     playlists = get_playlists(user_id)
@@ -23,6 +27,9 @@ def get_all_songs(user_id):
     for pl in playlists:
         songs += get_songs_from_playlist(user_id, pl)
     return songs
+
+def reset_playlist(user_id, playlist_id):
+    songs = get_songs_from_playlist(user_id, playlist_id)
 
 if __name__=="__main__":
     USER_ID = '22zrcabx54xzpwfktbhgok3nq'
