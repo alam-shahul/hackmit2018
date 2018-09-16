97e4d378f2584ac8ac3d12d2681847b18c8dffbc
Jen Li <jingyuli@mit.edu>
2018-09-15 23:26:12 -0400

    weights

--- final_pipeline/sort.py
+++ final_pipeline/sort.py
@@ -1,13 +1,16 @@
-import spotipy
+import spotipy as sp
 import spotipy.util as util
 
 from spotipy.oauth2 import SpotifyClientCredentials
 
+import ui
+
 client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+SP = sp.Spotify(client_credentials_manager=client_credentials_manager)
 
 def sort_songs_by_feature(track_list, feature='danceability'):
 	features = []
 	for song_num in range(0, len(track_list), 50):
-		features += spotipy.audio_features(track_list[song_num:song_num + 50])
-	print features
+		features += SP.audio_features(track_list[song_num:song_num + 50])
+	zip(track_list, features)
+	print(features)
